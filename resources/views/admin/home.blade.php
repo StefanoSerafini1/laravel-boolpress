a253dccca768d23b76e21f8331b21b778042dcf2
StefanoSerafini <stefanoserafini7@gmail.com>
2021-02-01 18:28:53 +0100

    modifiche

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,8 +8,7 @@
                 <h1 class="text-center text-danger mb-3 mt-3">Admin Area</h1>
                 <blockquote class="blockquote">
                     <div class="container">
-                        <p class="mb-3 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus deserunt nihil minima dignissimos cupiditate animi rem dicta itaque. Corporis sit aperiam adipisci tempora sapiente cum facilis, ipsam odio facere autem!</p>
-                        <p class="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus deserunt nihil minima dignissimos cupiditate animi rem dicta itaque. Corporis sit aperiam adipisci tempora sapiente cum facilis, ipsam odio facere autem!</p>
+                       <p>zona riservata admin</p>
                     </div>
                 </blockquote>
             </div>
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,9 +3,6 @@
 @section('content')
 <div class="container">
     <h1 class="text-center text-danger mb-3 mt-3">{{ $post->title }}</h1>
-
     <div class="text"> {{ $post->body }}</div>
-    <a href="{{route('admin.posts.edit', Auth::id())}}" class="btn btn-primary">Edit</a>
-
 </div>
 @endsection
