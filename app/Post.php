bb1ac085b8e06cc1a0f84f7b140b0c040279f386
StefanoSerafini <stefanoserafini7@gmail.com>
2021-02-01 18:04:22 +0100

    creazione post funzionante

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    /**
+     * MASS ASSIGNMENT
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body',
+        'slug'
+    ];
+
+    /**
+     * DB REALATIONS
+     */
+
+     //users - posts
+     public function user() {
+         return $this->belongsTo('App\User');
+     }
 }
